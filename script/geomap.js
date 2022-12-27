82b6273ad9f3540bac87ca67547aae2942767799
SAID Raoufdine <said.raoufdine@gmail.com>
2022-12-27 12:48:47 +0100

    Copie de la visualisation du notebook observable à la github page

---
+++ script/geomap.js
@@ -0,0 +1,170 @@
+//  return selected causes
+function get_list_of_selected_cause_geomap() {
+  const list_all_cause = get_all_causes();
+  var list_cause = [];
+
+  list_all_cause.forEach(cause => {
+    if (document.getElementById("geomap-" + cause).checked)
+      list_cause.push(cause);
+  });
+
+  return list_cause;
+}
+
+//  draw geomap by number of death
+function draw_geo_map(data, list_cause, year, log_scale = false) {
+  const margin = ({top: 0, right: 200, bottom: 0, left: 0})
+  const w = 800
+  const h = 400
+
+  const min_deaths = Math.max(get_min_sum_deaths(data, list_cause), 1);
+  const max_deaths = get_max_sum_deaths(data, list_cause);
+  
+  const dataForYear = data.filter(e => e.Year.getFullYear() == year);
+
+  const logScale = d3.scaleLog().domain([min_deaths, max_deaths])
+  const colorScaleLog = d3.scaleSequential((d) => d3.interpolateReds(logScale(d)))
+  const linearScale = d3.scaleLinear().domain([min_deaths, max_deaths])
+  const colorScaleLinear = d3.scaleSequential((d) => d3.interpolateReds(linearScale(d)))
+
+  const colorScale = log_scale ? colorScaleLog : colorScaleLinear;
+  
+  const svg = d3.select("#geomapw").attr("height", h).attr("width", w)
+  
+  //  clear svg
+  svg.selectAll('*').remove()
+  
+  const projection = d3.geoNaturalEarth1()
+    .scale((w - margin.right - margin.left) / 5.5)
+    .translate([w / 2 - margin.right + margin.left, h / 2])
+
+  //  draw map
+  svg.selectAll("path")
+     .data(get_data_map().features).enter().append("path")
+     .attr("fill", e => {
+       const codePays = e.id;
+       const nb_deaths = get_sum_deaths(dataForYear, year, codePays, list_cause);
+       if (nb_deaths > 0) {
+         return colorScale(nb_deaths);
+       }
+       return "#ccc";
+     })
+     .attr("d", d3.geoPath().projection(projection))
+  .style("stroke", "#fff")
+
+  //  draw legend
+  var legend_h = 7*20;
+  var legend_w = 10;
+  var sample = [min_deaths == 0 ? 1 : min_deaths];
+  var etapes = [0];
+  if (log_scale) {
+    var last_x = 0;
+    var i = 0;
+    for (let x = 1; Math.exp(x) < max_deaths; x = x * 1.02) {
+      sample.push(min_deaths + Math.exp(x));
+      if (x - last_x > 1) {
+        if (i % 2 == 1 && i != 1 && (sample.length - 1) - etapes[etapes.length - 1] >= 10)
+          etapes.push(sample.length - 1);
+        last_x = x;
+        ++i;
+      }
+    }
+  } else {
+    var step = (max_deaths - min_deaths) / legend_h;
+    for (let x = min_deaths + step; x < max_deaths; x += step)
+      sample.push(x);
+    for (let i = sample.length / 6; i < sample.length; i += sample.length / 6)
+      etapes.push(Math.floor(i))
+  }
+  sample.push(max_deaths);
+  if ((sample.length - 1) - etapes[etapes.length - 1] >= 10)
+    etapes.push(sample.length - 1);
+  else
+    etapes[etapes.length - 1] = sample.length - 1;
+  sample.reverse();
+
+  svg.selectAll("line")
+     .data(sample).enter()
+     .append("line")
+     .attr("y1", (_, i) => 20 + i)
+     .attr("y2", (_, i) => 20 + i)
+     .attr("x1", w - margin.right + 10)
+     .attr("x2", w - margin.right + 10 + legend_w)
+     .attr("stroke", c => colorScale(c))
+
+  etapes.forEach((v, i) => {
+    var y = v * (sample.length - 1) / etapes[etapes.length - 1];
+
+    svg.append('line')
+       .style("stroke", "black")
+       .attr("x1", w - margin.right + 10 + legend_w)
+       .attr("y1", 20 + (sample.length - 1) - y)
+       .attr("x2", w - margin.right + 10 + legend_w + 5)
+       .attr("y2", 20 + (sample.length - 1) - y);
+
+    svg.append("text")
+       .attr("x", w - margin.right + 10 + legend_w + 5 + 3)
+       .attr("y", 20 + (sample.length - 1) - y + 4)
+       .text(round_by_first_digit(Math.round(sample[Math.max(0, Math.min(sample.length - v, sample.length - 1))])).toString())
+       .style("font-size", "12px");
+
+    svg.append("rect")
+       .attr("x", w - margin.right + 10)
+       .attr("y", 20 + 150)
+       .attr("width", legend_w)
+       .attr("height", legend_w)
+       .attr("fill", "#ccc");
+
+    svg.append("text")
+       .attr("x", w - margin.right + 10 + legend_w + 5 + 3)
+       .attr("y", 20 + 150 + 10)
+       .text("0 ou données absentes")
+       .style("font-size", "12px")
+  })
+}
+
+//  main geomap
+function geomap_main() {
+  //  List cause js
+  const list_all_cause = get_all_causes();
+  list_all_cause.sort();
+  var l = document.getElementById("list_cause");
+
+  l.innerHTML = "";
+  
+  for (let i = 0; i < list_all_cause.length; ++i) {
+    l.innerHTML +=
+      '<li><input type="checkbox" id="geomap-' + list_all_cause[i] + '" ' + (i == 0 ? 'checked' : '') + '>' + list_all_cause[i] + '</li>';
+  }
+
+  list_all_cause.forEach(cause => {
+    document.getElementById("geomap-" + cause).addEventListener("change",  (ev) => {
+      //  garder aux moins une cause coché
+      if (get_list_of_selected_cause_geomap().length == 0)
+        document.getElementById("geomap-" + cause).checked = true;
+      draw_geo_map(get_data_raw(), get_list_of_selected_cause_geomap(), $("#slider_geomap").val(), document.getElementById("logscale_geomap").checked)
+    })
+  });
+  
+  var checkList = document.getElementById('list_cause_parent');
+  checkList.getElementsByClassName('anchor')[0].onclick = function(evt) {
+    if (checkList.classList.contains('visible'))
+      checkList.classList.remove('visible');
+    else
+      checkList.classList.add('visible');
+  }
+
+  //  logscale checkbox js
+  var checkbox = document.getElementById("logscale_geomap");
+
+  checkbox.addEventListener("change", (ev) => {
+    draw_geo_map(get_data_raw(), get_list_of_selected_cause_geomap(), $("#slider_geomap").val(), document.getElementById("logscale_geomap").checked)
+  });
+
+  draw_geo_map(get_data_raw(), get_list_of_selected_cause_geomap(), $("#slider_geomap").val(), document.getElementById("logscale_geomap").checked)
+
+  d3.select("#slider_geomap").node().addEventListener(window.navigator.userAgent.indexOf("MSIE ") ? "change" : "input", (ev) => {
+    draw_geo_map(get_data_raw(), get_list_of_selected_cause_geomap(), $("#slider_geomap").val(), document.getElementById("logscale_geomap").checked);
+  });
+}
+
---
+++ script/main.js
@@ -0,0 +1,156 @@
+//  data
+var DATA_RAW = null;
+var DATA_MAP = null;
+var ALL_CAUSES = [];
+var LIST_CODE = [];
+var LIST_YEAR = [];
+var MIN_YEAR = 0;
+var MAX_YEAR = 0;
+
+//  return dataset
+function get_data_raw() {
+  return DATA_RAW;
+}
+
+//  return data of geomap
+function get_data_map() {
+  return DATA_MAP;
+}
+
+//  return all list of causes
+function get_all_causes() {
+  return ALL_CAUSES;
+}
+
+//  return list code
+function get_list_code() {
+  return LIST_CODE;
+}
+
+//  return list year
+function get_list_year() {
+  return LIST_YEAR;
+}
+
+//  return min year
+function get_min_year() {
+  return MIN_YEAR;
+}
+
+//  return max year
+function get_max_year() {
+  return MAX_YEAR;
+}
+
+//  round by first digit (ex: 123456 => 100000)
+function round_by_first_digit(n) {
+  var sign = 1;
+
+  if (n < 0)
+    sign = -1;
+
+  var n_s = n.toString();
+  var tmp = n_s[0] + ".";
+
+  for (let i = 1; i < n_s.length; ++i) {
+    tmp += n_s[i];
+  }
+
+  var res = Math.round(Number(tmp)) + "";
+  for (let i = 0; i < n_s.length - 1; ++i)
+    res += "0";
+
+  return Number(res);
+}
+
+//  sum death for a line
+function get_sum_deaths_line(line, list_cause) {
+  var sum = 0;
+
+  list_cause.forEach(cause => sum += line[cause]);
+
+  return sum;
+}
+
+//  max sum deaths
+function get_max_sum_deaths(data, list_cause) {
+  return data.reduce((a, line) => Math.max(get_sum_deaths_line(line, list_cause), a), get_sum_deaths_line(data[0], list_cause));
+}
+
+//  min sum deaths
+function get_min_sum_deaths(data, list_cause) {
+  return data.reduce((a, line) => Math.min(get_sum_deaths_line(line, list_cause), a), get_sum_deaths_line(data[0], list_cause));
+}
+
+//  sum deaths by year and country code
+function get_sum_deaths(data, year, code, list_cause) {
+  var dataForYear = data.filter(e => e.Year.getFullYear() == year);
+  var infoPays = dataForYear.filter(e => e.Code == code);
+
+  if (infoPays.length == 0)
+    infoPays = {};
+  else
+    infoPays = infoPays[0];
+
+  var sum = 0;
+
+  for (let i = 0; i < list_cause.length; ++i) {
+    sum += infoPays[list_cause[i]];
+  }
+
+  return sum;
+}
+
+//  load data
+async function load_data() {
+  //  dataset
+  DATA_RAW = await d3.csv("https://raw.githubusercontent.com/Baragouine/visualization-number-of-deaths-by-cause-around-world-per-year/main/data/cause_of_deaths_with_population.csv",
+    d => {
+      for (const property in d) {
+        if (property != "Country/Territory" &&
+          property != "Code" &&
+          property != "Year") {
+          d[property] = +d[property]
+        }
+      }
+      d["Year"] = d3.utcParse("%Y")(d["Year"])
+      return d
+    }
+  )
+
+  //  geomap data
+  DATA_MAP = await d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson");
+
+  //  list of all causes
+  for (const property in DATA_RAW[0]) {
+    if (property != "Country/Territory" &&
+      property != "Code" &&
+      property != "Year" &&
+      property != "Population") {
+      ALL_CAUSES.push(property);
+    }
+  }
+
+  //  list of codes
+  LIST_CODE = new Set();
+  DATA_RAW.forEach(e => LIST_CODE.add(e.Code));
+  LIST_CODE = [...LIST_CODE];
+
+  //  list of years
+  LIST_YEAR = new Set();
+  DATA_RAW.forEach(e => LIST_YEAR.add(e.Year.getFullYear()));
+  LIST_YEAR = [...LIST_YEAR];
+
+  //  min and max year
+  MIN_YEAR = Math.min(get_list_year());
+  MAX_YEAR = Math.max(get_list_year());
+}
+
+//  main
+async function main() {
+  await load_data();
+  
+  //  run geomap main
+  geomap_main();
+}
+
